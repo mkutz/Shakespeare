742bcca03378f06c690c7aeebff9a6b66d743265
Michael Kutz <michael.kutz@rewe-digital.com>
2021-04-07 15:14:58 +0200

    Turn BrowseTheWeb into a record

--- modules/selenium/src/main/java/io/github/mkutz/shakespeare/selenium/BrowseTheWeb.java
+++ modules/selenium/src/main/java/io/github/mkutz/shakespeare/selenium/BrowseTheWeb.java
@@ -3,18 +3,18 @@ package io.github.mkutz.shakespeare.selenium;
 import io.github.mkutz.shakespeare.Ability;
 import org.openqa.selenium.WebDriver;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * {@link Ability} to browse the web using a Selenium {@link WebDriver}.
  */
-public class BrowseTheWeb implements Ability, AutoCloseable {
-
-    private final WebDriverSupplier webDriverSupplier;
+public record BrowseTheWeb(WebDriverSupplier webDriverSupplier) implements Ability, AutoCloseable {
 
     /**
      * @param webDriverSupplier the {@link WebDriverSupplier} used to setup the {@link WebDriver}
      */
-    public BrowseTheWeb(WebDriverSupplier webDriverSupplier) {
-        this.webDriverSupplier = webDriverSupplier;
+    public BrowseTheWeb {
+        requireNonNull(webDriverSupplier);
     }
 
     /**
