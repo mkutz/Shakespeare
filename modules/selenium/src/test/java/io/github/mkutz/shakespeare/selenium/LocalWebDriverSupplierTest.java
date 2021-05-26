73d488007b6fbfd904dbcafd49c190c9383583bc
Michael Kutz <michael.kutz@rewe-digital.com>
2021-05-26 10:55:02 +0200

    Improve LocalWebDriverSupplierTest

--- modules/selenium/src/test/java/io/github/mkutz/shakespeare/selenium/LocalWebDriverSupplierTest.java
+++ modules/selenium/src/test/java/io/github/mkutz/shakespeare/selenium/LocalWebDriverSupplierTest.java
@@ -2,63 +2,52 @@ package io.github.mkutz.shakespeare.selenium;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.safari.SafariDriver;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.io.IOException;
+import java.util.Map;
 
-import static io.github.mkutz.shakespeare.selenium.BrowserType.CHROME;
-import static io.github.mkutz.shakespeare.selenium.BrowserType.FIREFOX;
-import static io.github.mkutz.shakespeare.selenium.BrowserType.SAFARI;
+import static io.github.mkutz.shakespeare.selenium.BrowserType.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class LocalWebDriverSupplierTest {
 
-    @Test
-    @DisplayName("get returns a ChromeDriver")
-    void getTest1() throws IOException, InterruptedException {
-        assumeTrue(new ProcessBuilder("which", "google-chrome").start().waitFor() == 0);
+    private static final Map<BrowserType, String> COMMANDS = Map.of(
+            CHROME, "google-chrome",
+            FIREFOX, "firefox",
+            SAFARI, "safari");
 
-        final var localWebDriverSupplier = new LocalWebDriverSupplier(
-                CHROME, new ChromeOptions().setHeadless(true));
+    @ParameterizedTest(name = "get returns a {0}")
+    @EnumSource(BrowserType.class)
+    void getTest1(BrowserType browserType) throws IOException, InterruptedException {
+        assumeTrue(COMMANDS.containsKey(browserType));
+        assumeTrue(new ProcessBuilder("which", COMMANDS.get(browserType)).start().waitFor() == 0);
+
+        final var localWebDriverSupplier = new LocalWebDriverSupplier(browserType);
 
         assertThat(localWebDriverSupplier.get())
                 .isNotNull()
-                .isExactlyInstanceOf(ChromeDriver.class);
+                .isExactlyInstanceOf(browserType.getWebDriverClass());
 
         localWebDriverSupplier.close();
     }
 
     @Test
-    @DisplayName("get returns a FirefoxDriver")
-    void getTest2() throws IOException, InterruptedException {
-        assumeTrue(new ProcessBuilder("which", "firefox").start().waitFor() == 0);
+    @DisplayName("get caches the WebDriver")
+    void getTest2() {
+        final var localWebDriverSupplier = new LocalWebDriverSupplier(FIREFOX);
 
-        final var localWebDriverSupplier = new LocalWebDriverSupplier(
-                FIREFOX, new FirefoxOptions().setHeadless(true));
-
-        assertThat(localWebDriverSupplier.get())
-                .isNotNull()
-                .isExactlyInstanceOf(FirefoxDriver.class);
+        final var webDriver = localWebDriverSupplier.get();
+        assertThat(webDriver).isSameAs(localWebDriverSupplier.get());
 
         localWebDriverSupplier.close();
     }
 
     @Test
-    @DisplayName("get returns a SafariDriver")
-    void getTest3() throws IOException, InterruptedException {
-        assumeTrue(new ProcessBuilder("which", "safari").start().waitFor() == 0);
-
-        final var localWebDriverSupplier = new LocalWebDriverSupplier(SAFARI);
-
-        assertThat(localWebDriverSupplier.get())
-                .isNotNull()
-                .isExactlyInstanceOf(SafariDriver.class);
-
-        localWebDriverSupplier.close();
+    @DisplayName("close does nothing when no WebDriver was created")
+    void closeTest1() {
+        new LocalWebDriverSupplier(FIREFOX).close();
     }
 }
