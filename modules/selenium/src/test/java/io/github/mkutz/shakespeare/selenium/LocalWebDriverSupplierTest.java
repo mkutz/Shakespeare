1fd32206b0bcc81ee70fc5a6b6290c1e56f4f4a9
Michael Kutz <michael.kutz@rewe-digital.com>
2021-05-26 11:21:40 +0200

    Fix LocalWebDriverSupplierTest to work on CI

--- modules/selenium/src/test/java/io/github/mkutz/shakespeare/selenium/LocalWebDriverSupplierTest.java
+++ modules/selenium/src/test/java/io/github/mkutz/shakespeare/selenium/LocalWebDriverSupplierTest.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.IOException;
 import java.util.Map;
 
-import static io.github.mkutz.shakespeare.selenium.BrowserType.*;
+import static io.github.mkutz.shakespeare.selenium.BrowserType.CHROME;
+import static io.github.mkutz.shakespeare.selenium.BrowserType.FIREFOX;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -16,8 +20,11 @@ class LocalWebDriverSupplierTest {
 
     private static final Map<BrowserType, String> COMMANDS = Map.of(
             CHROME, "google-chrome",
-            FIREFOX, "firefox",
-            SAFARI, "safari");
+            FIREFOX, "firefox");
+
+    private static final Map<BrowserType, Capabilities> HEADLESS = Map.of(
+            CHROME, new ChromeOptions().setHeadless(true),
+            FIREFOX, new FirefoxOptions().setHeadless(true));
 
     @ParameterizedTest(name = "get returns a {0}")
     @EnumSource(BrowserType.class)
@@ -25,7 +32,7 @@ class LocalWebDriverSupplierTest {
         assumeTrue(COMMANDS.containsKey(browserType));
         assumeTrue(new ProcessBuilder("which", COMMANDS.get(browserType)).start().waitFor() == 0);
 
-        final var localWebDriverSupplier = new LocalWebDriverSupplier(browserType);
+        final var localWebDriverSupplier = new LocalWebDriverSupplier(browserType, HEADLESS.get(browserType));
 
         assertThat(localWebDriverSupplier.get())
                 .isNotNull()
