b05fa6468bffb6435a58d1866cf85603113a622a
Michael Kutz <michael.kutz@rewe-digital.com>
2021-09-13 17:30:32 +0200

    Use WebDriverManager 5 instead of Testcontainers

--- modules/selenium/src/main/java/org/shakespeareframework/selenium/DockerWebDriverSupplier.java
+++ modules/selenium/src/main/java/org/shakespeareframework/selenium/DockerWebDriverSupplier.java
@@ -1,27 +1,16 @@
 package org.shakespeareframework.selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testcontainers.containers.BrowserWebDriverContainer;
-
-import java.util.EnumMap;
-import java.util.Map;
 
 /**
- * {@link WebDriverSupplier} relying on <a href="https://www.testcontainers.org/">Testcontainers</a>'
- * {@link BrowserWebDriverContainer} to run browsers on a local Docker infrastructure.
+ * A {@link WebDriverSupplier} for Docker based browser instances. Uses {@link WebDriverManager} to manage the container
+ * and create the {@link WebDriver}.
  */
 public class DockerWebDriverSupplier extends WebDriverSupplier {
 
-    /**
-     * {@link Map} of running {@link BrowserWebDriverContainer}s mapped to their {@link BrowserType}. Generally it is
-     * possible to run multiple containers in parallel however, as each container will have a significant need for
-     * memory and CPU time, it should ideally be limited to one only!
-     */
-    private static final Map<BrowserType, BrowserWebDriverContainer<?>> webDriverContainers =
-            new EnumMap<>(BrowserType.class);
-
+    private final WebDriverManager webDriverManager;
     private WebDriver webDriver;
 
     /**
@@ -30,6 +19,10 @@ public class DockerWebDriverSupplier extends WebDriverSupplier {
      */
     public DockerWebDriverSupplier(BrowserType browserType, Capabilities additionalCapabilities) {
         super(browserType, additionalCapabilities);
+        this.webDriverManager = WebDriverManager
+                .getInstance(browserType.getWebDriverClass())
+                .capabilities(getCapabilities())
+                .browserInDocker();
     }
 
     /**
@@ -41,27 +34,15 @@ public class DockerWebDriverSupplier extends WebDriverSupplier {
 
     @Override
     public WebDriver get() {
-        final var browserType = getBrowserType();
-        final var capabilities = getCapabilities();
-        var webDriverContainer = webDriverContainers.get(browserType);
-        if (webDriverContainer == null || !webDriverContainer.isRunning()) {
-            webDriverContainer = new BrowserWebDriverContainer<>().withCapabilities(capabilities);
-            webDriverContainer.start();
-            webDriverContainers.put(browserType, webDriverContainer);
-        }
         if (webDriver == null) {
-            webDriver = new RemoteWebDriver(
-                    webDriverContainer.getSeleniumAddress(),
-                    capabilities);
+            webDriver = webDriverManager.create();
         }
         return webDriver;
     }
 
     @Override
     public void close() {
-        if (webDriver != null) {
-            webDriver.quit();
-            webDriver = null;
-        }
+        webDriverManager.quit();
+        webDriver = null;
     }
 }
