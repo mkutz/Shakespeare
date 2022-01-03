c42686f8a46e226d93373452bf0e8eea3eee456f
Michael Kutz <michael.kutz@rewe-digital.com>
2022-01-03 08:32:58 +0100

    Add CallHttpApis Abilitiy

---
+++ modules/retrofit/src/main/java/org/shakespeareframework/retrofit/HeaderInterceptor.java
@@ -0,0 +1,44 @@
+package org.shakespeareframework.retrofit;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * {@link Interceptor} to add headers to any request. This should be used to set authentication or other contextual
+ * headers.
+ */
+public class HeaderInterceptor implements Interceptor {
+
+    private final Map<String, String> headers = new HashMap<>();
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        final var originalRequest = chain.request();
+        if (headers.isEmpty()) {
+            return chain.proceed(originalRequest);
+        }
+
+        final var requestBuilder = originalRequest.newBuilder();
+        headers.forEach(requestBuilder::addHeader);
+        return chain.proceed(requestBuilder.build());
+    }
+
+    /**
+     * @param key   name of the header to be added
+     * @param value value of the header to be added
+     */
+    public void add(String key, String value) {
+        this.headers.put(key, value);
+    }
+
+    /**
+     * @param key name of the header to be removed
+     */
+    public void remove(String key) {
+        headers.remove(key);
+    }
+}
