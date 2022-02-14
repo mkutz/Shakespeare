fde3b03be9a8341719ba28d3e82eb457b2b3bc39
Michael Kutz <mail@michael-kutz.de>
2022-02-14 17:15:58 +0100

    Remove unneeded files from pages branch

--- modules/core/src/main/java/org/shakespeareframework/Ability.java
+++
@@ -1,8 +0,0 @@
-package org.shakespeareframework;
-
-/**
- * An {@link Ability} can be {@link Actor#uses used} by an {@link Actor} to {@link Actor#does perform} a
- * {@link Task} or {@link Actor#checks answer} a {@link Question}.
- */
-public interface Ability {
-}
