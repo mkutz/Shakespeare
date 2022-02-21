b931aa2535f9f247cca9d94864c70b67340aed72
Documentor <mail@michael-kutz.de>
2022-02-21 15:41:11 +0000

    Update docs for main
    [skip ci]

--- docs/main/javadoc/retrofit/package-search-index.js
+++ docs/main/javadoc/retrofit/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.shakespeareframework.retrofit"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.shakespeareframework.retrofit"}]
