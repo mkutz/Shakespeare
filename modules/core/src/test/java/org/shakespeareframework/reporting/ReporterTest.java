0c90b8e47a6432f49ec8f7f4fafba883e2b88e08
Michael Kutz <mail@michael-kutz.de>
2022-03-17 19:10:24 +0100

    Increase coverage on Reporter interfaces

--- modules/core/src/test/java/org/shakespeareframework/reporting/ReporterTest.java
+++ modules/core/src/test/java/org/shakespeareframework/reporting/ReporterTest.java
@@ -9,22 +9,37 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 class ReporterTest {
 
     @Test
-    @DisplayName("each method has a noop default")
+    @DisplayName("each task method has a noop default")
     void test1() {
-        var defaultReporter = new Reporter() {};
+        var defaultReporter = new TaskReporter() {};
         var rachel = new Actor("Rachel");
         var task = new TestTaskBuilder().perform(actor -> {});
+        var retryableTask = new RetryableTestTaskBuilder().perform(actor -> {});
+
+        assertThatCode(() -> {
+                    defaultReporter.start(rachel, task);
+                    defaultReporter.retry(rachel, retryableTask, new RuntimeException("Fail!"));
+                    defaultReporter.success(rachel, task);
+                    defaultReporter.failure(rachel, task, new RuntimeException("Fail!"));
+                }
+        ).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("each question method has a noop default")
+    void test2() {
+        var defaultReporter = new QuestionReporter() {};
+        var rachel = new Actor("Rachel");
         var question = new TestQuestionBuilder<String>().answer(actor -> "answer");
         var retryableQuestion = new RetryableTestQuestionBuilder<String>().answer(actor -> "answer");
 
         assertThatCode(() -> {
-                    defaultReporter.start(rachel, task);
-                    defaultReporter.success(rachel, task);
                     defaultReporter.start(rachel, question);
                     defaultReporter.retry(rachel, retryableQuestion, "unacceptable");
                     defaultReporter.retry(rachel, retryableQuestion, new RuntimeException("Fail!"));
                     defaultReporter.success(rachel, retryableQuestion, "answer");
-                    defaultReporter.failure(rachel, task, new RuntimeException("Fail!"));
+                    defaultReporter.failure(rachel, retryableQuestion, "unacceptable");
+                    defaultReporter.failure(rachel, retryableQuestion, new RuntimeException("Fail!"));
                 }
         ).doesNotThrowAnyException();
     }
