2b7142e405de95c998862881d8b28e25e25662a8
Michael Kutz <mail@michael-kutz.de>
2022-04-15 12:25:32 +0200

    Remove unused import

--- modules/core/src/test/java/org/shakespeareframework/RetryableQuestionTest.java
+++ modules/core/src/test/java/org/shakespeareframework/RetryableQuestionTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.MethodSource;
 
 class RetryableQuestionTest {
 
