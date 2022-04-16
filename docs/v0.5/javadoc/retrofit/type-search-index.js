fdb538ee8469ec19d5d4426c5836ea79324d3c6e
Documentor <mail@michael-kutz.de>
2022-04-16 10:27:07 +0000

    Update docs for v0.5
    [skip ci]

---
+++ docs/v0.5/javadoc/retrofit/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.shakespeareframework.retrofit","l":"CallHttpApis.Builder"},{"p":"org.shakespeareframework.retrofit","l":"CallHttpApis"},{"p":"org.shakespeareframework.retrofit","l":"HeaderInterceptor"}]
