1a12f0ac5720f85d6bf61dc474d025d6af4bfea5
Michael Kutz <michael.kutz@rewe-digital.com>
2022-10-27 18:20:09 +0200

    Simplify and test Oauth2Token
    #70

--- modules/retrofit/src/main/java/org/shakespeareframework/retrofit/oauth2/Oauth2Token.java
+++ modules/retrofit/src/main/java/org/shakespeareframework/retrofit/oauth2/Oauth2Token.java
@@ -10,37 +10,19 @@ import java.time.Instant;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Oauth2Token {
 
-  private final String userId;
   private final String accessToken;
-  private final Integer expiresIn;
-  private final String refreshToken;
-  private final Integer refreshExpiresIn;
-  private final String scope;
   private final Instant expirationInstant;
 
   /**
    * @param accessToken the access token {@link String} for the Authorization header
    * @param expiresIn number of seconds before the token expires
-   * @param refreshToken the refresh token {@link String}
-   * @param refreshExpiresIn number of seconds before the {@link #refreshToken} expires
-   * @param scope scope of the {@link #accessToken}
-   * @param userId the ID of the user the {@link #accessToken} was issued for
    */
   @JsonCreator
   public Oauth2Token(
       @JsonProperty("access_token") String accessToken,
-      @JsonProperty("expires_in") Integer expiresIn,
-      @JsonProperty("refresh_token") String refreshToken,
-      @JsonProperty("refresh_expires_in") Integer refreshExpiresIn,
-      @JsonProperty("scope") String scope,
-      @JsonProperty("user_id") String userId) {
+      @JsonProperty("expires_in") Integer expiresIn) {
     this.accessToken = accessToken;
-    this.expiresIn = expiresIn;
     this.expirationInstant = now().plusSeconds(expiresIn);
-    this.refreshToken = refreshToken;
-    this.refreshExpiresIn = refreshExpiresIn;
-    this.scope = scope;
-    this.userId = userId;
   }
 
   /**
@@ -49,7 +31,7 @@ public class Oauth2Token {
    * @return true, this is expired or will be expired in less than 10 seconds
    */
   public boolean isExpired() {
-    return expirationInstant.isAfter(now().minusSeconds(10));
+    return now().plusSeconds(10).isAfter(expirationInstant);
   }
 
   public String getAccessToken() {
