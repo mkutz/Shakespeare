008b4883ba02d0b830015d40d6ef3e6826bca526
Marlis Lamp <67092705+marlis-l@users.noreply.github.com>
2023-12-19 08:25:10 +0100

    Allow predicate in retryable task/question
    There are cases where the class of an exception isn't sufficient
    to decide weather it should be ignored or not.

--- modules/core/src/main/java/org/shakespeareframework/RetryableTask.java
+++ modules/core/src/main/java/org/shakespeareframework/RetryableTask.java
@@ -4,8 +4,8 @@ import java.util.Set;
 
 /**
  * A {@link Task} that will be retried until it succeeds or the {@link Retryable#getTimeout()
- * timeout} is reached. {@link Exception}s thrown will be ignored by default unless they are
- * contained in the {@link #getAcknowledgedExceptions() acknowledgedExceptions}.
+ * timeout} is reached. {@link Exception}s thrown will be ignored by default unless they match
+ * {@link #isAcknowledgedException(Exception) the acknowledged exception predicate}.
  */
 public interface RetryableTask extends Task, Retryable {
 
@@ -15,5 +15,13 @@ public interface RetryableTask extends Task, Retryable {
    */
   default Set<Class<? extends Exception>> getAcknowledgedExceptions() {
     return Set.of();
+  }
+
+  /**
+   * @return the predicate that decides weather the exception won't be ignored when thrown in a
+   *     retry
+   */
+  default boolean isAcknowledgedException(Exception exception) {
+    return getAcknowledgedExceptions().stream().anyMatch(acknowledge -> acknowledge.isInstance(exception));
   }
 }
